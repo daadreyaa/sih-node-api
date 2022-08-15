2fe5ea6c7549b4236663d4dd2dce4de85653c926
Daadreyaa <daadreyaa@gmail.com>
2022-08-15 23:57:54 +0530

    Update UserModal.js

--- models/UserModal.js
+++ models/UserModal.js
@@ -4,7 +4,8 @@ const crypto = require('crypto');
 const UserSchema = mongoose.Schema({
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
